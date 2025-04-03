97a8112c070ddad29ab2dfde272920d16ba49d35
ChristianSC8 <christian.supo@upeu.edu.pe>
2025-04-03 11:03:00 -0500

    tareaaaa

---
+++ SysAlmacen/src/main/java/pe/edu/upeu/sysalmacen/control/PrimeroController.java
@@ -0,0 +1,28 @@
+package pe.edu.upeu.sysalmacen.control;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pe.edu.upeu.sysalmacen.modelo.Marca;
+import pe.edu.upeu.sysalmacen.repositorio.MyRegularRepository;
+import pe.edu.upeu.sysalmacen.repositorio.MyRepository;
+
+@RestController
+@RequestMapping("primero")
+public class PrimeroController {
+    @Autowired
+    MyRepository repository;
+    @GetMapping
+
+
+    public Marca Listar(){
+        Marca marca = new Marca();
+        marca.setNombre("Primero Prueba");
+        marca.setIdMarca(1);
+        return repository.guardar(marca);
+
+    }
+
+    }
+
